745b4de9f7bca9d77efb7edc1915aa90ac147327
pavel kuzin <pavlushka3390@mail.ru>
2021-06-05 14:00:22 +0300

    создание домашки по ООП

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("hello")
+class Student:
+    def __init__(self, name, surname, gender):
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.finished_courses = []
